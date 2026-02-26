ddcaa74d38e449223d6cf1f4bbcae8bea63ac8bc
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-26 15:05:00 +0200

    Update featuredSlotRoutes.js

--- backend/src/routes/featuredSlotRoutes.js
+++ backend/src/routes/featuredSlotRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+const { getFeaturedSlots, assignFeaturedSlot, removeFeaturedSlot, rotateFeaturedSlot } = require('../controllers/featuredSlotController');
 
+const router = express.Router();
+
+router.get('/', authMiddleware(['ADMIN']), getFeaturedSlots);
+router.post('/assign', authMiddleware(['ADMIN']), assignFeaturedSlot);
+router.post('/:slotId/remove', authMiddleware(['ADMIN']), removeFeaturedSlot);
+router.post('/:slotId/rotate', authMiddleware(['ADMIN']), rotateFeaturedSlot);
+
+module.exports = router;
