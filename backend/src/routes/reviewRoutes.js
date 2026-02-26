e644d3fdce8787411591c71e8517e5d689bb313d
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-26 15:04:36 +0200

    Update reviewRoutes.js

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+const { getReviews, moderateReview } = require('../controllers/reviewController');
 
+const router = express.Router();
+
+router.get('/', authMiddleware(['ADMIN']), getReviews);
+router.post('/:id/moderate', authMiddleware(['ADMIN']), moderateReview);
+
+module.exports = router;
