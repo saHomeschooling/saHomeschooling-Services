fedc2526354c66cb3be43420ade60c381ff09871
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-26 15:05:27 +0200

    Update statsRoutes.js

--- backend/src/routes/statsRoutes.js
+++ backend/src/routes/statsRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+const { getStats } = require('../controllers/statsController');
 
+const router = express.Router();
+
+router.get('/', authMiddleware(['ADMIN']), getStats);
+
+module.exports = router;
