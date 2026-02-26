6ffefcd0ab0bc630f78d56efd998906b4d6315b1
thobekazitha-gif <thobekazitha40@gmail.com>
2026-02-26 15:04:08 +0200

    Update providerRoutes.js

--- backend/src/routes/providerRoutes.js
+++ backend/src/routes/providerRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/authMiddleware');
+const { getProviders, getProviderById, updateProvider, approveProvider, rejectProvider } = require('../controllers/providerController');
 
+const router = express.Router();
+
+router.get('/', authMiddleware(['ADMIN']), getProviders);
+router.get('/:id', authMiddleware(['ADMIN', 'PROVIDER']), getProviderById);
+router.put('/:id', authMiddleware(['ADMIN', 'PROVIDER']), updateProvider);
+router.post('/:id/approve', authMiddleware(['ADMIN']), approveProvider);
+router.post('/:id/reject', authMiddleware(['ADMIN']), rejectProvider);
+
+module.exports = router;
